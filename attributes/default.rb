10694294d084c8903e1c34e4c97aeed8c666fe0b
Soul Rebel <set.krag@gmail.com>
2013-06-04 15:52:26 +0200

    implemented `['etckeeper']['git_remote_enabled']` attribute, to disable `git push`

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,6 +2,7 @@ default['etckeeper']['packages']   = %w{etckeeper git}
 default['etckeeper']['dir']        = "/etc/etckeeper"
 default['etckeeper']['config']     = "#{node['etckeeper']['dir']}/etckeeper.conf"
 default['etckeeper']['vcs']        = "git"
+default['etckeeper']['git_remote_enabled']   = true
 default['etckeeper']['git_host']   = "github.com"
 default['etckeeper']['git_port']   = 22
 default['etckeeper']['git_repo']   = "etckeeper"
