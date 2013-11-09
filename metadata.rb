b82fe0194d74328fa602c892ff472dbe9c8f34cb
Alexander Saharchuk <pioneer@saharchuk.com>
2013-11-09 16:53:09 +0200

    Update etckeeper at http://community.opscode.com/cookbooks/etckeeper/source

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "alexander@saharchuk.com"
 license          "Apache 2.0"
 description      "Installs/Configures etckeeper"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.2"
+version          "0.0.3"
 
 depends "cron"
 
