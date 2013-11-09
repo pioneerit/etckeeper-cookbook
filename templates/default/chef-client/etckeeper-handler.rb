be3e4c2b9ed83c222c068b1a00f4a00cb9b7071a
Peter Niederlag <netservice@niekom.de>
2013-11-09 15:08:14 +0100

    dummy report handler working

--- templates/default/chef-client/etckeeper-handler.rb
+++ templates/default/chef-client/etckeeper-handler.rb
@@ -1,22 +1,22 @@
 require 'rubygems'
 require 'chef/log'
-require 'chef'
+#require 'chef'
 
 module Etckeeper
-  class Report < Chef::Handler
+  class ClientHandler < Chef::Handler
     def report
 
       Chef::Log.info "Etckeeper::Report handler started"
      
       # only commit when status is success ???
       message = [
-        "chef-client status",
-        "#{host_name} #{prefix}.success #{run_status.success? ? 1 : 0}",
-        "#{host_name} #{prefix}.updated_resources_num #{run_status.updated_resources.length}",
+        "chef-client",
+        "status:#{run_status.success? ? 1 : 0}",
+        "numupdates:#{run_status.updated_resources.length}",
       ].join(" ")
 
       #File.open(tempfile, 'w') {|f| f.write(message)}
-      Chef::Log.debug "git persist change to etc/" 
+      Chef::Log.info "git persist change to etc/ #{message}" 
       #Chef::Log.debug `#{node.zabbix.install_dir}/bin/zabbix_sender --config #{node.zabbix.etc_dir}/zabbix_agentd.conf --input-file #{tempfile}`
     end
   end
