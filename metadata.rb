3d19ba1ffc1bc67275e414a7a3136c5b53099481
Peter Niederlag <netservice@niekom.de>
2013-11-09 14:41:08 +0100

    add chef_handler in metadata

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,7 @@ description      "Installs/Configures etckeeper"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.2"
 
+depends "chef_handler"
 depends "cron"
 
 # TODO:
