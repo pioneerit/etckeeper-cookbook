7c372a6ec39ad8f657d29e1be8322708821b5d1c
Bastian Bringenberg <mail@bastian-bringenberg.de>
2013-11-10 11:08:25 +0100

    [etckeeper] removed git from package run and included git recipe

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,11 @@ maintainer_email "alexander@saharchuk.com"
 license          "Apache 2.0"
 description      "Installs/Configures etckeeper"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.4"
+version          "0.0.5"
 
 depends "chef_handler"
 depends "cron"
+depends "git"
 
 # TODO:
 # 1. support OS
--- recipes/default.rb
+++ recipes/default.rb
@@ -16,6 +16,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+include_recipe "git"
+
 node['etckeeper']['packages'].each do |pckg|
   package pckg
 end
