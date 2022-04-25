de57efdee602a36925bde1c81337b2ec431df05a
Jeremy MAURO <jeremy.mauro@gmail.com>
2022-04-25 12:18:25 +0200

    test: Udpate tests to comply to the newest chefspec
    - Remove old chefspec deprecated initialization
    - Update the ubuntu version to the latest (20.04)
    - Remove deprecated requirements
    
    Signed-off-by: Jeremy MAURO <jeremy.mauro@gmail.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,9 @@
 require 'chefspec'
-require 'chefspec/server'
 require 'chefspec/berkshelf'
 require 'chefspec/cacher'
 
 RSpec.configure do |config|
   config.platform = 'ubuntu'
-  config.version = '14.04'
+  config.version = '20.04'
   config.log_level = :error
 end
