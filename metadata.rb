de018e1fd0343e36f48329f86a32fd44bf7b545f
Oleksandr Sakharchuk <pioneer@saharchuk.com>
2022-04-27 17:55:27 +0200

    Update with latest quality checks and up to date details

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
 name             "etckeeper"
-maintainer       "Alexander Saharchuk and Steffen Gebert, Bastian Bringenberg, Peter Niederlag / TYPO3 Association, Jeremy Mauro"
+maintainer       "Oleksandr Sakharchuk and Steffen Gebert, Bastian Bringenberg, Peter Niederlag / TYPO3 Association, Jeremy Mauro"
 maintainer_email "alexander@saharchuk.com"
 license          "Apache 2.0"
 description      "Installs/Configures etckeeper"
@@ -8,6 +8,8 @@ version          "1.0.5"
 recipe           "etckeeper", "Install etckeeper and start local using etckeeper with etckeeper::config"
 recipe           "etckeeper::config", "Config etckeeper. Set VCS for local using. And add hooks if remote repo using"
 recipe           "etckeeper::commit", "Use with chef-run"
+issues_url       "https://github.com/pioneerit/etckeeper-cookbook/issues"
+source_url       "https://github.com/pioneerit/etckeeper-cookbook"
 
 %w{redhat centos scientific fedora debian ubuntu arch freebsd amazon gentoo}.each do |os|
   supports os
